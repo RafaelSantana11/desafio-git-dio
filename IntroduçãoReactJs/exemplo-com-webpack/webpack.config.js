22289b97e931b427d0f8260a0a34dba572290391
RafaelSantana11 <rafael@araguaiatec.com>
2022-01-10 09:12:07 -0300

    configurando webpack

---
+++ IntroduçãoReactJs/exemplo-com-webpack/webpack.config.js
@@ -0,0 +1,31 @@
+// Imports
+var path = require("path");
+
+// Plugins
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+// Exports
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundler.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    })
+  ]
+};
